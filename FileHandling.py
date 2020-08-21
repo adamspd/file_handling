0ee42fb8d42b71ede94630d45e59fe5162413c8e
Adams Pierre David <adamspierredavid@gmail.com>
2020-08-21 23:05:39 +0200

    Adding more functionality for the menu

--- FileHandling.py
+++ FileHandling.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import sys
+import sys, os, datetime
 from FunctionForFileHandling import *
 
 
@@ -12,6 +12,7 @@ def menu():
           "l: to list a directory\n",
           "date: to print modified date of a file\n",
           "t: to list the type of file in a directory\n",
+          "help: to have the possibility to know how each command works\n",
           "q: to quit\n"
           )
     com = input(": ")
@@ -19,7 +20,7 @@ def menu():
 
 
 com = menu()
-while com != "q":
+while True:
     if com == "c":
         filename = input("Name of the file: ")
         create_file(filename)
@@ -31,6 +32,26 @@ while com != "q":
     elif com == "del":
         filename = input("Name of the file to delete: ")
         remove_file(filename)
+        com = menu()
+    elif com == "date":
+        filename = input("Name of the file: ")
+        file_date(filename)
+        com = menu()
+    elif com == "t":
+        directory = input("Name of the directory to list type of file from: ")
+        type_file(directory)
+        print()
+        com = menu()
+    elif com == "l":
+        if not yes_or_no("Do you want to list a specific directory: "):
+            print(list_directory())
+            com = menu()
+        else:
+            directory = input("Name of directory or leave empty if current: ")
+            print(list_directory(directory))
+            com = menu()
+    elif com == "help":
+        helpme()
         com = menu()
     else:
         print("Unknown command ")
